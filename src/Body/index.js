daad8444ba697390a2b707e1731d2866a93ed6b3
Bogdan <flipi2017@mail.ru>
2023-03-24 17:59:20 +0600

    tabs change

--- src/Body/index.js
+++ src/Body/index.js
@@ -7,24 +7,19 @@ import './body.scss'
 function Body() {
     return (
         <Tabs
-            defaultActiveKey="profile"
+            defaultActiveKey="now"
             id="justify-tab-example"
             className="mb-3"
             justify
         >
-            <Tab eventKey="home" title="Home">
-                <TimeSelector id ='home' />
+            <Tab eventKey="now" title="Now">
+                <TimeSelector id ='now' />
                 <Map/>
             </Tab>
-            <Tab eventKey="profile" title="Profile">
-                <TimeSelector id='profile' />
+            <Tab eventKey="forecast" title="Forecast">
+                <TimeSelector id='forecast' />
                 <Map/>
             </Tab>
-            <Tab eventKey="longer-tab" title="Loooonger Tab">
-                <TimeSelector id = 'longer' />
-                <Map/>
-            </Tab>
-
         </Tabs>
     );
 }
